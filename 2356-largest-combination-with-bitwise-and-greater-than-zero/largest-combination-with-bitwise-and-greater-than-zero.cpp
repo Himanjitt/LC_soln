d51b2ef9b8e94392c049bc8d2aa2c4a45f81f3f7
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2024-11-07 18:47:43 +0530

    Time: 38 ms (29.10%) | Memory: 59.9 MB (99.40%) - LeetSync

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -3,7 +3,7 @@ public:
     int largestCombination(vector<int>& candidates) {
         int count = 0;
         int maxCount = 0;
-        for (int bitPos = 0; bitPos < 32; bitPos++) {
+        for (int bitPos = 0; bitPos < 24; bitPos++) {
             for (int i = 0; i < candidates.size(); i++) {
                 if ((candidates[i] & (1 << bitPos)) !=0) {
                     count++;
