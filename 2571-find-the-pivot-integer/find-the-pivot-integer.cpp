56735be826f7213a09236f41a4e80b972c09fa16
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2024-12-11 00:17:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (24.08%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        int totalSum=n*(n+1)/2;
-        int leftSum=0;
-        int rightSum=0;
-        for(int i=1;i<=n;i++){
-            leftSum+=i;
-            rightSum=totalSum-leftSum + i;
-            if(leftSum==rightSum) return i;
+        int leftvalue=1,rightvalue=n;
+        int leftSum=leftvalue,rightSum=rightvalue;
+        if(n==1) return 1;
+
+        while(leftvalue<rightvalue){
+
+            if(leftSum<rightSum){
+                leftvalue++;
+                leftSum+=leftvalue;
+            }else{
+                rightvalue--;
+                rightSum+=rightvalue;
+            }
+            if(leftSum==rightSum && leftvalue+1==rightvalue-1){
+                return leftvalue+1;
+            }
         }
         return -1;
     }
