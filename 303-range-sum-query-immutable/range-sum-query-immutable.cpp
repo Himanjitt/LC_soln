6ad72de4447d572764d62b234f39c88ce38128ab
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2024-12-13 19:07:26 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (15.81%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -1,12 +1,19 @@
 class NumArray {
 public:
-    vector<int>  nums;
+    vector<int> prefix;
     NumArray(vector<int>& nums) {
-        this->nums=nums;
+        prefix.resize(nums.size());
+        prefix[0] = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
     }
     
     int sumRange(int left, int right) {
-        return accumulate(nums.begin()+left, nums.begin()+right+1, 0);
+       if (left == 0) {
+            return prefix[right];
+        }
+        return prefix[right] - prefix[left - 1];
     }
 };
 
