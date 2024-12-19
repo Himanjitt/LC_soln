39ead9d4bf8a5f302cb45f2631fce7d4fdcbed95
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2024-12-19 09:25:12 +0530

    Time: 14 ms (44.51%) | Memory: 64.8 MB (70.15%) - LeetSync

--- 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.cpp
+++ 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size(), m = nums2.size();
-        unordered_map<int,int>mp1,mp2;
+        vector<int>result;
+        int count1 = 0;
+        int count2 = 0;
+        
+        
+        for(auto i : nums1){
+        
+            if(find(nums2.begin(),nums2.end(),i)!=nums2.end()) count1++;
+        }
 
-        for(auto n: nums1){
-            mp1[n]++;
+        for(auto i : nums2){
+            
+            if(find(nums1.begin(),nums1.end(),i)!=nums1.end()) count2++;
+            
         }
-        for(auto n: nums2){
-            mp2[n]++;
-        }
-        vector<int> ans;
-        int count1=0,count2=0;
-        for(int i=0;i<n;i++){
-            if(mp2.count(nums1[i])>0){
-                count1+=mp2[nums1[i]];
-                mp2[nums1[i]]=0;
-            }
-        }
-        for(int i=0;i<m;i++){
-            if(mp1.count(nums2[i])>0){
-                count2+=mp1[nums2[i]];
-                mp1[nums2[i]]=0;
-            }
-        }
-        ans.push_back(count2);
-        ans.push_back(count1);
 
-        return ans;
+        return {count1,count2};
     }
 };
