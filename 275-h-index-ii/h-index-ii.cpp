c87874cffa78cae18b9012bb5d88cebfa5a03d8b
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2024-12-20 14:04:33 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (20.04%) - LeetSync

--- 275-h-index-ii/h-index-ii.cpp
+++ 275-h-index-ii/h-index-ii.cpp
@@ -2,12 +2,19 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         int n=citations.size();
-        for(int i=0;i<n;i++){
-            int h=n-i;
-            if(citations[i]>=h){
-                return h;
+        int start=0;
+        int end=n-1;
+        int ans=0;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            int h=n-mid;
+            if(citations[mid]>=h){
+                ans=h;
+                end=mid-1;
+            }else{
+                start=mid+1;
             }
         }
-        return 0;
+        return ans;
     }
 };
