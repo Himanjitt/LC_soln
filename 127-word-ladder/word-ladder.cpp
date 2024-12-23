c884e86359b52c25ae1a27d5d86a0d5599bfd8a8
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2024-12-23 18:48:17 +0530

    Time: 89 ms (54.15%) | Memory: 19.6 MB (83.85%) - LeetSync

---
+++ 127-word-ladder/word-ladder.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord,
+                     vector<string>& wordList) {
+
+        unordered_set<string> st(wordList.begin(), wordList.end());
+
+        int n=beginWord.size();
+        if (wordList.size() == 0)
+            return 0;
+        queue<string> q;
+        q.push(beginWord);
+        if (beginWord== endWord) {
+            return 0;
+        }
+        vector<char> alpha(26);
+
+        for(int i=0;i<26;i++){
+            alpha.push_back(97+i);
+        }
+
+        int steps = 1;
+        while (!q.empty()) {
+            int size = q.size();
+            while (size--) {
+                string node = q.front();
+                q.pop();
+
+                if (node == endWord) {
+                    return steps;
+                }
+                for (int i = 0; i < n; i++) {
+                    char ch = node[i];
+                    for (char it : alpha) {
+                        node[i] = it;
+                        if (st.count(node) == 1) {
+                            q.push(node);
+                            st.erase(node);
+                        }
+                    }
+                    node[i] = ch;
+                }
+            }
+            steps++;
+        }
+        return 0;
+    }
+};
