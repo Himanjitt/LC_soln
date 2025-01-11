270db9a21cdb2f26552f067b0fe7201646ef512e
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-01-11 11:29:21 +0530

    Time: 7 ms (91.64%) | Memory: 70.7 MB (52.35%) - LeetSync

--- 456-132-pattern/132-pattern.cpp
+++ 456-132-pattern/132-pattern.cpp
@@ -6,6 +6,8 @@ public:
         int nums3=INT_MIN;
         stack<int>st;
 
+        // here we did reverse traversal because we meed to take care of the 
+        // largest index
         for(int i=n-1;i>=0;i--){
             if(nums[i]<nums3){
                 return true;
