7cd348eebe8c4b197406ea8fc83d99edf0bdc777
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-01-12 09:42:28 +0530

    Time: 31 ms (4.42%) | Memory: 103.8 MB (10.55%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isPresent(char ch, string &ans){
+        for(char ele: ans){
+            if(ele==ch){
+                return true;
+            }
+        }
+
+        return false;
+    }
+    bool isPresentAfter(int i, string s,char ch){
+        for(int j=i;j<s.size();j++){
+            if(s[j]==ch){
+                return true;
+            }
+        }
+
+        return false;
+    }
+    string removeDuplicateLetters(string s) {
+        string ans;
+
+        for(int i=0;i<s.size();i++){
+
+            while(ans.empty()==false && ans.back() > s[i] && isPresentAfter(i,s,ans.back())==true
+                && isPresent(s[i],ans)==false){
+                    ans.pop_back();    
+            }
+            if(isPresent(s[i],ans)==false)
+                ans.push_back(s[i]);
+        }
+
+        return ans;
+    }
+};
