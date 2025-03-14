2eb1b1e2c953ce737f2359a0d6aae45e386e2cb4
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-03-14 22:50:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (19.06%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.cpp
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        //search range is 1-num
-        for(long long i=1;i<=num;i++){
-            if(i*i==num){
+        // search range is 1-num
+        int start = 1, end = num;
+        while (start <= end) {
+            long long mid = start + (end - start) / 2;
+
+            if (mid * mid == num) {
                 return true;
+            } else if (mid * mid < num) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
 
