60e1c13a2bec5c5a4d5065f2c250a68e3cfc7c0f
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-03-16 08:52:59 +0530

    Time: 2 ms (37.00%) | Memory: 96 MB (10.56%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& arr1, vector<int>& arr2) {
-        for (int i = 0; i < arr2.size(); i++) {
-            arr1.push_back(arr2[i]);
+
+        int n1 = arr1.size(), n2 = arr2.size();
+        int i = 0, j = 0;
+        vector<int> arr3;
+        while (i < n1 && j < n2) {
+            if (arr1[i] < arr2[j]) {
+                arr3.push_back(arr1[i]);
+                i++;
+            } else {
+                arr3.push_back(arr2[j]);
+                j++;
+            }
         }
-        sort(arr1.begin(), arr1.end());
-        int n = arr1.size();
+        while (i < n1) {
+            arr3.push_back(arr1[i]);
+            i++;
+        }
+        while (j < n2) {
+            arr3.push_back(arr2[j]);
+            j++;
+        }
+        int n = arr3.size();
         if (n % 2 == 1) {
-            return arr1[n / 2];
+            return arr3[n / 2];
         } else {
             double sum = 0;
-            sum = arr1[n / 2] + arr1[n / 2 - 1];
+            sum = arr3[n / 2] + arr3[n / 2 - 1];
             return sum / 2;
         }
 
