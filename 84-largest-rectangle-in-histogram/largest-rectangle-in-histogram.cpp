26002f7eb69274d672c60c30502ade04766e7868
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-03-25 00:51:34 +0530

    Time: 34 ms (27.26%) | Memory: 95.6 MB (7.59%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    void findNS(vector<int>& NS, vector<int>& heights) {
+        stack<int> st1;
+        for (int i = 0; i < heights.size(); i++) {
+            while (st1.empty() == false && heights[i] < heights[st1.top()]) {
+                NS[st1.top()] = i;
+                st1.pop();
+            }
+            st1.push(i);
+        }
+    }
+
+    void findPS(vector<int>& PS, vector<int>& heights) {
+        stack<int> st2;
+        for (int i = heights.size() - 1; i >= 0; i--) {
+            while (st2.empty() == false && heights[i] < heights[st2.top()]) {
+                PS[st2.top()] = i;
+                st2.pop();
+            }
+            st2.push(i);
+        }
+    }
+    
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> NS(n, n);
+        vector<int> PS(n, -1);
+        vector<int> width(n);
+        vector<int> area(n);
+        findNS(NS, heights);
+        findPS(PS, heights);
+        int maxi = 0;
+        for (int i = 0; i < n; i++) {
+            width[i] = NS[i] - PS[i] - 1;
+            area[i] = width[i] * heights[i];
+            maxi = max(maxi, area[i]);
+        }
+        return maxi;
+    }
+};
