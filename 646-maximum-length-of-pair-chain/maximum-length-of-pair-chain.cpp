aa5fe0db96745c86b7c0fdcd37e425ebb9c7d0c6
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-06-11 01:54:43 +0530

    Time: 51 ms (43.08%) | Memory: 27.2 MB (53.41%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -1,32 +1,20 @@
 // sorting greedily because it is given any order, unlike normal LIS
 class Solution {
 public:
-    int solve(vector<vector<int>>& nums, int ind, int prevInd, vector<vector<int>> &dp) {
-        // pruning
-
-        // base case
-        if (ind == nums.size()) {
-            return 0;
+    
+    int findLongestChain(vector<vector<int>>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        vector<int> t(n,1);
+        int maxLis=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j][1] < nums [i][0]){
+                    t[i]=max(t[i],t[j] + 1);
+                    maxLis=max(maxLis, t[i]);
+                }
+            }
         }
-        //to normalize since prevInd can be -1
-        if(dp[ind][prevInd + 1] != -1){
-            return dp[ind][prevInd + 1];
-        }
-        // choices  
-        // not take
-        int notTake = solve(nums, ind + 1, prevInd, dp);
-
-        // take
-        int take = 0;
-        if ((prevInd == -1) || nums[ind][0] > nums[prevInd][1]) {
-            take = 1 + solve(nums, ind + 1, ind, dp);
-        }
-        return dp[ind][prevInd + 1] = max(notTake, take);
-    }
-    int findLongestChain(vector<vector<int>>& pairs) {
-        sort(pairs.begin(),pairs.end());
-        int n=pairs.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return solve(pairs, 0, -1, dp);
+        return maxLis;
     }
 };
