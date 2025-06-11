642583d9273cc12cfd614188e1343e7d6901b03a
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-06-11 12:34:39 +0530

    Time: 27 ms (96.29%) | Memory: 18.6 MB (86.69%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -1,57 +1,48 @@
-//check notes
+// check notes
 class Solution {
 public:
-    static bool func(string &word1, string &word2){
+    static bool func(string& word1, string& word2) {
         return word1.size() < word2.size();
     }
 
-    bool isPredecessor(string &currWord, string &prevWord){
-        if(currWord.size() - prevWord.size() != 1){
+    bool isPredecessor(string& currWord, string& prevWord) {
+        if (currWord.size() - prevWord.size() != 1) {
             return false;
         }
-        //isSubsequece leetcode 392
-        int iC=0, jP=0;
+        // isSubsequece leetcode 392
+        int iC = 0, jP = 0;
 
-        while(iC < currWord.size()){
-            if(prevWord[jP] ==  currWord[iC]){
+        while (iC < currWord.size()) {
+            if (prevWord[jP] == currWord[iC]) {
                 jP++;
                 iC++;
-            }else{
+            } else {
                 iC++;
             }
         }
-        if(jP == prevWord.size()) {
+        if (jP == prevWord.size()) {
             return true;
         }
         return false;
     }
-    int solve(vector<string>& words, int ind, int prevInd, vector<vector<int>>&dp){
-        //pruning
 
-        //base case
-        if(ind == words.size()){
-            return 0;
-        }
-
-        if(dp[ind][prevInd + 1] != -1){
-            return dp[ind][prevInd + 1];
-        }
-        //not take
-        int notTake = solve(words, ind+1, prevInd,dp);
-
-        //take
-        int take=0;
-        if(prevInd == -1 || isPredecessor(words[ind], words[prevInd]) == true){
-            take= 1 + solve(words, ind+1, ind, dp);
-        }
-
-        return dp[ind][prevInd + 1] = max(notTake, take);
-    }
     int longestStrChain(vector<string>& words) {
-        int n=words.size();
-        //custom sorting based on length
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        sort(words.begin(),words.end(), func);
-        return solve(words, 0, -1, dp);
+        int n = words.size();
+        // custom sorting based on length
+        vector<int> dp(n,1);
+        sort(words.begin(), words.end(), func);
+
+        // standard optimized way
+        int maxlen = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j< i; j++) {
+
+                if (isPredecessor(words[i], words[j]) == true) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                    maxlen = max(maxlen, dp[i]);
+                }
+            }
+        }
+        return maxlen;
     }
 };
