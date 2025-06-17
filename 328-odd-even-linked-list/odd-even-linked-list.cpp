79765ca57eebd6e10f806453d654ff378974ccf2
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-06-17 21:01:37 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (87.97%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -11,40 +11,22 @@
 class Solution {
    public:
     ListNode* oddEvenList(ListNode* head) {
-        if(head == NULL) return NULL;
-        ListNode* dummy = new ListNode(-1);
-        ListNode* oddPointer = head;
-        if(head->next == NULL){
+        if(head == NULL || head->next == NULL){
             return head;
         }
+
+        ListNode* oddPointer = head;
         ListNode* evenPointer = head->next;
-        ListNode* curr = dummy;
-        while (oddPointer != NULL && oddPointer->next != NULL) {
-            ListNode* temp = new ListNode(oddPointer->val);
-            curr->next = temp;
-            curr = curr->next;
-            
-            oddPointer = oddPointer->next->next;
-            
-        }
-        if(oddPointer){
-            ListNode* temp = new ListNode(oddPointer->val);
-            curr->next = temp;
-            curr = curr->next;
-        }
-        while (evenPointer != NULL && evenPointer->next != NULL) {
-            ListNode* temp = new ListNode(evenPointer->val);
-            curr->next = temp;
-            curr = curr->next;
-             
-            evenPointer = evenPointer->next->next;
-        }
-        if(evenPointer){
-            ListNode* temp = new ListNode(evenPointer->val);
-            curr->next = temp;
-            curr = curr->next;
+        ListNode* evenHead = head->next;
+
+        while(evenPointer && evenPointer->next){
+            oddPointer->next = oddPointer->next->next;
+            evenPointer->next = evenPointer->next->next;
+            oddPointer= oddPointer->next;
+            evenPointer = evenPointer->next;
         }
 
-        return dummy->next;
+        oddPointer->next = evenHead;
+        return head;
     }
 };
