35a55beeb4a993fbc09e2418e880cebd5a181861
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-06-21 18:06:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (18.32%) - LeetSync

---
+++ 91-decode-ways/decode-ways.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isValid(string& temp) {
+        if (temp[0] == '0') {
+            return false;
+        }
+        int num = stoi(temp);
+        if (num >= 1 && num <= 26) {
+            return true;
+        }
+
+        return false;
+    }
+    int solve(string& s, int i, vector<int>& dp) {
+        // base case
+        if (i == s.size()) {
+            return 1;
+        }
+
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+
+        int count = 0;
+        for (int k = 1; k <= 2; k++) {
+            string temp = "";
+            if (i + k <= s.size()) {
+                temp = s.substr(i, k);
+
+                if (isValid(temp) == true) {
+                    count += solve(s, i + k, dp);
+                }
+            }
+        }
+
+        return dp[i] = count;
+    }
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int> dp(n, -1);
+        return solve(s, 0, dp);
+    }
+};
