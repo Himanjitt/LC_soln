a1d2ff4ef032cb73f714b9930b1760be4d7e81f5
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-06-23 11:01:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (31.04%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool solve(string &s, string &p, int i, int j){
+    bool solve(string &s, string &p, int i, int j, vector<vector<int>>&dp){
         if(j == p.size()){
             //both should end at same time
             if(i == s.size()){
@@ -9,29 +9,34 @@ public:
             return false;
         }
 
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
         if(p[j+1] == '*'){
             //not take
-            bool notTake = solve(s, p, i, j+2);
+            bool notTake = solve(s, p, i, j+2, dp);
             //take
             bool take = false;
             if((i < s.size()) && (s[i] == p[j] || p[j] == '.')){
-                take = solve(s, p, i+1, j);
+                take = solve(s, p, i+1, j, dp);
             }
 
-            return notTake || take;
+            return dp[i][j] = notTake || take;
         }
 
         //else means normal case
         bool elseOP = false;
         if((i < s.size()) && (s[i] == p[j] || p[j] == '.')){
-            elseOP = solve(s, p, i+1, j+1);
+            elseOP = solve(s, p, i+1, j+1, dp);
         }
-        return elseOP;
+        return dp[i][j] =  elseOP;
     }
 
 
     bool isMatch(string s, string p) {
-        
-        return solve(s, p, 0, 0);
+        int n = s.size(), m = p.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return solve(s, p, 0, 0, dp);
     }
 };
