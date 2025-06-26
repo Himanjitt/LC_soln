119f5165e5e09ff17424cadec08fb479b96b521d
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-06-26 16:52:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (70.90%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.cpp
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
-    int maxDistance(vector<int>& colors) {
-        int n = colors.size();
+    int maxDistance(vector<int>& nums) {
+        int n = nums.size();
         int ans = 0;
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(colors[i] != colors[j]){
-                    ans = max(ans,j-i);
-                }
-            }
+
+        //greedily maximum distance is only possible 
+        //from start or end
+        int maxDistLeft = 0;
+        for(int i=1; i<n; i++){
+            if(nums[i] != nums[0])
+                maxDistLeft = max(maxDistLeft, i - 0);
         }
 
-        return ans;
+        int maxDistRight = 0;
+        for(int i=n-2; i>= 0; i--){
+            if(nums[i] != nums[n-1])
+                maxDistLeft = max(maxDistLeft, n-1 - i);
+        }
+
+        return max(maxDistLeft, maxDistRight);
     }
 };
