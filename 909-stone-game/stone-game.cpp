ca918bf83c5e31c8f378299702158a5fc1d47017
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-06-26 23:03:30 +0530

    Time: 20 ms (14.94%) | Memory: 19.5 MB (35.22%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
+    int solve(int i, int j, vector<int>&piles, vector<vector<int>>&dp){
+        if(i > j){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        //alice picks starting - bob pick rest
+        int startPick = piles[i] - solve(i+1, j, piles, dp);
+
+        //alice picks ending - bob pick rest
+        int endPick = piles[j] - solve(i, j-1, piles, dp);
+
+        return dp[i][j] = max(startPick, endPick);
+
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        //substract alice score - lee score
+        int n= piles.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        int score = solve(0, n-1, piles, dp);
+        if(score > 0) return true;
+
+        return false;
     }
 };
