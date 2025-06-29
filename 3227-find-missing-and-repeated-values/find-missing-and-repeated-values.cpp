68e12f78db32e7815ac9304e736e71e9d05f687f
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-06-29 16:25:30 +0530

    Time: 0 ms (100.00%) | Memory: 25.6 MB (90.65%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& nums) {
-        int n = nums.size();
-        unordered_map<int, int> mp;
-        vector<int> ans(2);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                mp[nums[i][j]]++;
-                if (mp[nums[i][j]] == 2) {
-                    ans[0] = nums[i][j];
-                }
-            }
-        }
-        for (int i = 1; i <= n * n; i++) {
-            if (mp.find(i) == mp.end()) {
-                ans[1] = i;
-            }
-        }
+        int m = nums.size();
+        long long n = m * m;
 
-        return ans;
+        long long SN = (n * (n + 1)) / 2;
+
+        long long S2N = (n * (n + 1) * (2 * n + 1)) / 6;
+
+        long long S = 0, S2 = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                S += nums[i][j];
+                S2 += (long long)nums[i][j] * (long long)nums[i][j];
+            }
+        }
+        //x-y
+        long long val1 = S - SN;
+        //(x-y)(x+y)
+        long long val2 = S2 - S2N;
+
+        //(x+y)
+        val2 = val2 / val1;
+
+        long long x = (val1 + val2) / 2;
+        long long y = x - val1;
+
+        return {(int)x, (int)y};
     }
 };
