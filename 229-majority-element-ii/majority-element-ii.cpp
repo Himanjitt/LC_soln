1e64e37ca6def116e4161094cd3f045aa9434602
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-06-29 15:50:29 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (75.56%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,21 +1,42 @@
+// Moore Voting Algo
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        int F=n/3;
+        int n = nums.size();
+        int ele1 = INT_MIN, ele2 = INT_MIN;
+        int count1 = 0, count2 = 0;
 
-        unordered_map<int,int>mp;
-
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-        }
-        vector<int> ans;
-        for(auto it: mp){
-            if(it.second > F){
-                ans.push_back(it.first);
+        for (int i = 0; i < n; i++) {
+            if (count1 == 0 && nums[i] != ele2) {
+                ele1 = nums[i];
+                count1 = 1;
+            } 
+            else if (count2 == 0 && nums[i] != ele1) {
+                ele2 = nums[i];
+                count2 = 1;
+            }
+             else if (nums[i] == ele1)
+                count1++;
+            else if (nums[i] == ele2)
+                count2++;
+            else {
+                count1--;
+                count2--;
             }
         }
 
+        int ele1Count=0, ele2Count=0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == ele1) {
+                ele1Count++;
+            } 
+            else if (nums[i] == ele2) {
+                ele2Count++;
+            }
+        }
+        vector<int> ans;
+        if (ele1Count > (n / 3)) ans.push_back(ele1);
+        if (ele2Count > (n / 3)) ans.push_back(ele2);
         return ans;
 
     }
