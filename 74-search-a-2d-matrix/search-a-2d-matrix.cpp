3225dc956f2a43a6864330d4cab79c951968a33e
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-06-30 14:34:08 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (41.98%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& mat, int target) {
-        for (auto vec : mat) {
-            int start = 0, end = vec.size() - 1;
-            while (start <= end) {
-                int mid = start + (end - start) / 2;
-                if (vec[mid] == target) {
-                    return true;
-                } else if (vec[mid] > target) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
+        int m = mat.size(), n = mat[0].size();
+        
+        int end = (m * n) - 1;
+        int start = 0;
+        while (start <= end) {
+            int mid = start+(end - start) / 2;
+            int row = mid / n;
+            int col = mid % n;
+            if (mat[row][col] == target) {
+                return true;
+            }
+            else if (mat[row][col] > target){
+                end = mid -1;
+            }
+            else{
+                start = mid +1;
             }
         }
-        return false;
+
+        return false;;
     }
 };
