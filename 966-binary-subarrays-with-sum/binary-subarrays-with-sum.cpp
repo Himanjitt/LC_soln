6b69b59e1131d0c54aee885053f98b04a12d6676
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-06-30 17:12:12 +0530

    Time: 15 ms (34.98%) | Memory: 38.8 MB (18.86%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int k) {
+        // prefix sum to frequency
+        unordered_map<int, int> mp;
+        int n = nums.size();
+        int sum = 0;
+        mp[0]=1;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum +=nums[i];
+
+            int rem=sum-k;
+            if(mp.find(rem)!=mp.end()){
+                count+=mp[rem];
+            }
+            mp[sum]++;
+        }
+        return count;
+    }
+};
