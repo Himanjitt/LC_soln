195ee0395aea3756d6f48261fde07befe020c361
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-07-02 13:31:37 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (19.98%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -2,35 +2,35 @@ class Solution {
 public:
     bool search(vector<int>& nums, int target) {
         int n = nums.size();
-        int start =0, end = n-1;
+        int start = 0, end = n - 1;
 
-        while(start <= end){
-            int mid = start + (end - start)/2;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
 
-            if(nums[mid] == target)
+            if (nums[mid] == target)
                 return true;
-            
-            //shrink search space in case duplicates
-            if(nums[start] == nums[mid] && nums[mid] == nums[end]){
+
+            // shrink search space in case duplicates
+            while (start < end && nums[start] == nums[mid] &&
+                   nums[end] == nums[mid]) {
                 start++;
                 end--;
-                continue;
             }
 
-            //left sorted 
-            if(nums[start] <= nums[mid]){
-                if(target >= nums[start] && target <= nums[mid]){
-                    end = mid -1;
-                }else{
-                    start = mid +1;
+            // left sorted
+            if (nums[start] <= nums[mid]) {
+                if (target >= nums[start] && target <= nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
                 }
             }
             // right sorted
-            else{
-                if(target >= nums[mid] && target <= nums[end]){
-                    start = mid +1;
-                }else{
-                    end = mid -1;
+            else {
+                if (target >= nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
                 }
             }
         }
