9c64e5f28b4101d24326b73a347cd5fe70e187df
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-07-03 16:11:41 +0530

    Time: 16 ms (62.18%) | Memory: 47.2 MB (61.36%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -4,41 +4,41 @@
 // no. of subarray that it will be minimum is a*b
 class Solution {
 public:
-  
     int sumSubarrayMins(vector<int>& arr) {
-        const int mod=1000000007;
-        int n=arr.size();
-        long long total=0;
-        vector<int>nse(n,n);
-        vector<int>pse(n,-1);
-        stack<int>st;
+        const int mod = 1000000007;
+        int n = arr.size();
+        long long total = 0;
+        vector<int> nse(n, n);
+        vector<int> pse(n, -1);
+        stack<int> st;
 
-       // nse 
-       for(int i=0;i<n;i++){
-        while(!st.empty() && arr[st.top()]>=arr[i]){
-            nse[st.top()]=i;
-            st.pop();
-            
-        } 
-        st.push(i);
-       }
-
-       //PSE
-        stack<int>st1;
-
-        for(int i=n-1;i>=0;i--){
-        while(!st1.empty() && arr[i]<arr[st1.top()]){
-            pse[st1.top()]=i;
-            st1.pop();  
+        // nse
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && arr[st.top()] >= arr[i]) {
+                int top = st.top();
+                st.pop();
+                nse[top] = i;
+            }
+            st.push(i);
         }
-        st1.push(i);
-       }
 
+        // PSE
+        stack<int> st1;
 
-        for(int i=0;i<n;i++){
-            int left=i-pse[i];
-            int right=nse[i]-i;
-              total = (total + (long long)right * left % mod * arr[i] % mod) % mod;
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st1.empty() && arr[i] < arr[st1.top()]) {
+                int top = st1.top();
+                st1.pop();
+                pse[top] = i;
+            }
+            st1.push(i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            int left = i - pse[i];
+            int right = nse[i] - i;
+            total =
+                (total + (long long)right * left % mod * arr[i] % mod) % mod;
         }
         return total;
     }
