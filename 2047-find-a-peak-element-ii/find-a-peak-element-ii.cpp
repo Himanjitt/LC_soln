129f04d2b418f2fba55e56f284a4a20bef9ec87c
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-07-03 11:59:40 +0530

    Time: 0 ms (100.00%) | Memory: 51 MB (5.62%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -1,34 +1,44 @@
+//idea is to find the max in a particular row (row would be chose by BS)
+//check its up and down is less or not and according eliminate the rows
 class Solution {
 public:
-    int findMaxColIndex(vector<int>& arr) {
-        int maxiInd = -1;
-        int maxi = INT_MIN;
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr[i] > maxi) {
-                maxi = arr[i];
-                maxiInd = i;
+    int findInd(vector<int> nums){
+        int maxi =nums[0];
+        int maxInd = 0;
+
+        for(int i =0;i<nums.size();i++){
+            if(nums[i] > maxi){
+                maxi = nums[i];
+                maxInd = i;
             }
         }
 
-        return maxiInd;
+        return maxInd;
     }
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        int r = mat.size();
-        int c = mat[0].size();
-        int start = 0, end = r - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            int maxColIndex = findMaxColIndex(mat[mid]);
-            int up = (mid - 1 < 0) ? -1 : mat[mid - 1][maxColIndex];
-            int down = (mid + 1 >= r) ? -1 : mat[mid + 1][maxColIndex];
-            if (mat[mid][maxColIndex] > up && mat[mid][maxColIndex] > down) {
-                return {mid, maxColIndex};
-            } else if (down > mat[mid][maxColIndex]) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+        int r = mat.size(), c = mat[0].size();
+        int start = 0, end = r-1;
+
+        
+        while(start <= end){
+            int mid = start+ (end - start)/2;
+
+            int maxColInd = findInd(mat[mid]);
+            
+            int up = (mid - 1 < 0) ? -1 : mat[mid-1][maxColInd];
+            int down = (mid + 1 >= r ) ? -1 : mat[mid+1][maxColInd];
+            
+            if(mat[mid][maxColInd] > up && mat[mid][maxColInd] > down){
+                return {mid, maxColInd};
+            }
+            else if(mat[mid][maxColInd] < up){
+                end = mid -1;
+            }
+            else{
+                start = mid +1;
             }
         }
+
         return {-1, -1};
     }
 };
