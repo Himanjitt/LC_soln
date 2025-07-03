d6721fa92f8830ebbdf24da3550be7256cedeb3f
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-07-03 18:13:06 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (59.12%) - LeetSync

--- 556-next-greater-element-iii/next-greater-element-iii.cpp
+++ 556-next-greater-element-iii/next-greater-element-iii.cpp
@@ -1,8 +1,32 @@
+// variation of next permutation
 class Solution {
 public:
+    void nextPermutation(string &nums) {
+        //find the breaking point
+        int n = nums.size();
+        int breakInd=-1;
+        for(int i = n-2;i >= 0;i--){
+            if(nums[i] < nums[i+1]){
+                breakInd = i;
+                break;
+            }
+        }
+        if(breakInd == -1){
+            reverse(nums.begin(), nums.end());
+            return ;
+        }
+        for(int i= n-1;i>=breakInd;i--){
+            if(nums[breakInd] < nums[i]){
+                swap(nums[breakInd], nums[i]);
+                break;
+            }
+        }
+        reverse(nums.begin() + breakInd + 1, nums.end());
+    }
+
     int nextGreaterElement(int n) {
         string s = to_string(n);
-        next_permutation(s.begin(),s.end());
+        nextPermutation(s);
         long long next = stoll(s);
         if(next <= n || next > INT_MAX){
             return -1;
