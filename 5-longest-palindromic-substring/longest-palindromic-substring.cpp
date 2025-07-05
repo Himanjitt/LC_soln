cb652b7f65b2bbb557d85f8d6744e3fd3d6cea65
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-07-05 22:52:45 +0530

    Time: 1199 ms (5.42%) | Memory: 292.1 MB (9.20%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int start, maxLength;
+    int start = 0, maxLength = 1;
 
     bool isPalindrome(const string& s, int i, int j) {
         while (i < j) {
@@ -12,11 +12,14 @@ public:
         return true;
     }
 
-    void solve(int i, int j, const string& s, vector<vector<int>> &dp) {
+    void solve(int i, int j, const string& s, vector<vector<int>>& dp) {
         if (i > j)
             return;
-        if (dp[i][j] != -1)
+
+        if (dp[i][j] != -1) // Already visited
             return;
+
+        dp[i][j] = 1; // Mark as visited
 
         if (isPalindrome(s, i, j)) {
             if (j - i + 1 > maxLength) {
@@ -25,19 +28,14 @@ public:
             }
         }
 
-        dp[i][j] = isPalindrome(s, i, j) ? 1 : 0;
         solve(i + 1, j, s, dp);
         solve(i, j - 1, s, dp);
     }
 
     string longestPalindrome(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        start = 0;
-        maxLength = 1;
-
+        vector<vector<int>> dp(n, vector<int>(n, -1)); // memo table to track visited (i, j)
         solve(0, n - 1, s, dp);
-
         return s.substr(start, maxLength);
     }
 };
