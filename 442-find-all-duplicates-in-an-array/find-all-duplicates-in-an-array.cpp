3e812fa36f4f859f1bfcf674b9ddf904a4cbf113
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-07-08 09:58:03 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (99.62%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -1,15 +1,29 @@
+/* 
+it is sure that every integer occurs and at most twice
+therefor by pigeon hole eveyone will be visited
+
+ 0  1  2  3  4  5  6  7
+[4, 3, 2, 7, 8, 2, 3, 1]   => [1,n] => index from [0,n-1]
+-4,-3,-2,-7      ,-3,-1
+
+here 2 is pointing at index 1 2times therefore it will be +ve
+the second time we find value at index 1 to be -ve hence 2 occured 2 times
+
+*/
+
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        vector<int> result; // Result vector to store duplicate numbers
-        for (int i = 0; i < nums.size(); ++i) {
-            int index = abs(nums[i]) - 1; // Get the index corresponding to the number
-            if (nums[index] < 0) { // If the number at index is negative, it means it's seen before
-                result.push_back(index + 1); // Add the duplicate number to the result
-            } else {
-                nums[index] = -nums[index]; // Mark the number as seen by negating it
-            }
+        vector<int> ans;
+
+        for(int ele : nums){
+            //if already -ve , we have visited 2nd time
+            if(nums[abs(ele) - 1] < 0) {
+                ans.push_back(abs(ele));
+            } 
+            nums[abs(ele) - 1] = (-1) * nums[abs(ele)-1];
         }
-        return result; // Return the vector containing duplicate numbers
+
+        return ans;
     }
 };
