e48585e12ffbdab43774b7ceaa0c736091ea73c6
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-07-11 23:00:10 +0530

    Time: 121 ms (16.28%) | Memory: 140.6 MB (16.07%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,11 +1,10 @@
+// MCM DP // Partition DP
 class Solution {
 public:
     int n;
-    bool isPali(int index, int i,string &s){
-        int start=index,end=i;
-
-        while(start<end){
-            if(s[start]!=s[end]){
+    bool isPali(int start, int end, string& s) {
+        while (start < end) {
+            if (s[start] != s[end]) {
                 return false;
             }
             start++;
@@ -14,25 +13,26 @@ public:
 
         return true;
     }
-    
-    void solve(vector<vector<string>>&ans, string &s, int index,vector<string>temp){
-        if(index >= s.size()){
+
+    void solve(vector<vector<string>>& ans, string& s, int i,
+               vector<string> temp) {
+        if (i >= s.size()) {
             ans.push_back(temp);
         }
 
-        for(int i=index;i<n;i++){
-            if(isPali(index,i,s)){
-                temp.push_back(s.substr(index,i-index+1));
-                solve(ans,s,i+1,temp);
+        for (int k = i; k < n; k++) {
+            if (isPali(i, k, s)) {
+                temp.push_back(s.substr(i, k - i + 1));
+                solve(ans, s, k + 1, temp);
                 temp.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
-        n=s.size();
-        vector<vector<string>>ans;
-        vector<string>temp;
-        solve(ans,s,0,temp);
+        n = s.size();
+        vector<vector<string>> ans;
+        vector<string> temp;
+        solve(ans, s, 0, temp);
         return ans;
     }
 };
