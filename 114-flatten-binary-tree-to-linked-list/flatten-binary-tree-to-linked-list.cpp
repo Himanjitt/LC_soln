f34dfd74b36d748d23f21f8d7ec2dd2fc3bf9188
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-07-13 00:28:08 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (50.43%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -11,32 +11,25 @@
  */
 class Solution {
 public:
-    TreeNode* prev = NULL;
-    
-    void preorder(TreeNode* curr){
-        if(curr == NULL){
-            return;
-        }
-
-        if(prev){
-            prev->right = curr;
-            prev->left = NULL;
-        }
-        TreeNode* rightSub = curr->right;
-        prev = curr;
-        preorder(curr->left);
-        preorder(rightSub);
-    }
     void flatten(TreeNode* root) {
-        //traverse in preorder way, save the curr node
-        //if prevNode is NULL then pass
-        //if not NULL then prev -> right will be the curr(note curr is in preorder traversal)
-        //prev->left = NULL;
-        //before moving curr save the right sub tree by storing it
-        //check notes
-        TreeNode* prev = NULL;
-        preorder(root);
+        TreeNode* curr = root;
 
-        
+        while(curr != NULL){
+            if(curr->left == NULL){
+                curr = curr->right;
+            }
+            else{
+                TreeNode* prev = curr->left;
+
+                while(prev->right != NULL){
+                    prev=prev->right;
+                }
+
+                prev->right= curr->right;
+                curr->right =curr->left;
+                curr->left = NULL;
+                curr = curr->right;
+            }
+        }
     }
 };
