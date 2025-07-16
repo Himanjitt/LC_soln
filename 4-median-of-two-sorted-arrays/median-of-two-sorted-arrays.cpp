d32db237fd6e191f92a630333222abba3543322e
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-07-16 20:30:28 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (78.66%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,38 +1,45 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& arr1, vector<int>& arr2) {
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if(nums1.size() > nums2.size()){
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int n1 = nums1.size(), n2= nums2.size();
+        int n = n1 + n2;
 
-        int n1 = arr1.size(), n2 = arr2.size();
-        int i = 0, j = 0;
-        vector<int> arr3;
-        while (i < n1 && j < n2) {
-            if (arr1[i] < arr2[j]) {
-                arr3.push_back(arr1[i]);
-                i++;
-            } else {
-                arr3.push_back(arr2[j]);
-                j++;
+        int left = (n1 + n2 +1) / 2;
+        int start = 0, end = n1;
+        
+        while(start <= end){
+            int mid1 = start + (end - start)/2;
+            int mid2 = left - mid1;
+
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
+
+            if(mid1 - 1 >= 0)l1  = nums1[mid1 - 1];
+            if(mid2 - 1>=0) l2   = nums2[mid2 - 1];
+            if(mid1 < n1)r1      = nums1[mid1];
+            if(mid2  < n2) r2    = nums2[mid2];
+
+            
+            if(l1 <= r2 && l2 <= r1){
+                if(n % 2 != 0) return max(l1, l2);
+
+                int ele1 = max(l1, l2);
+                int ele2 = min(r1, r2);
+
+                return double(ele1 + ele2) /2; 
             }
-        }
-        while (i < n1) {
-            arr3.push_back(arr1[i]);
-            i++;
-        }
-        while (j < n2) {
-            arr3.push_back(arr2[j]);
-            j++;
-        }
-        int n = arr3.size();
-        if (n % 2 == 1) {
-            return arr3[n / 2];
-        } else {
-            double sum = 0;
-            sum = arr3[n / 2] + arr3[n / 2 - 1];
-            return sum / 2;
+            else if(l1 > r2){// left search
+                end = mid1-1;
+            }
+            else{
+                start = mid1+1;
+            }
         }
 
         return -1;
-    }
-}
 
-;
+    }
+};
