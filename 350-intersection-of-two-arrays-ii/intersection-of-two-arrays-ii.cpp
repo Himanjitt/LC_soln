4378f7280f487ad08239ca27da0766c249ae583a
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-07-17 09:22:56 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (54.79%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,36 +1,19 @@
-/*
-[4, 9, 5]
-
-
-
-[9, 4, 9, 8, 4]
-
-  l
-4 5 9
-  r
-4 4 8 9 9
-
-*/
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end());
+        unordered_map<int,int>mp;
+        for(int num: nums1){
+            mp[num]++;
+        }
 
-        int n1 = nums1.size(), n2 = nums2.size();
-        int i =0, j=0;
         vector<int> ans;
-        while(i < n1 && j < n2 ){
-            if(nums1[i] == nums2[j]){
-                ans.push_back(nums1[i]);
-                i++;
-                j++;
-            }
-            else if(nums1[i] < nums2[j]){
-                i++;
-            }
-            else{
-                j++;
+        for(int i=0;i<nums2.size();i++){
+            if(mp.find(nums2[i]) != mp.end()){
+                ans.push_back(nums2[i]);
+                mp[nums2[i]]--;
+                if(mp[nums2[i]] == 0){
+                    mp.erase(nums2[i]);
+                }
             }
         }
 
