f0ecbb568996aae296da3c20ce76a6fbb6da6993
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-07-21 22:16:49 +0530

    Time: 279 ms (30.63%) | Memory: 168.1 MB (5.69%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -1,33 +1,31 @@
+
 class Solution {
 public:
-    long long solve(vector<int>& nums, int ind, bool isEven, vector<vector<long long>>&dp) {
-        if (ind == nums.size()) {
+    long long solve(vector<int>& nums, int i, bool isEven, vector<vector<long long>>&dp){
+        if(i == nums.size()){
             return 0;
         }
-
-        if(dp[ind][isEven] != -1){
-            return dp[ind][isEven];
+        if(dp[i][isEven] != -1){
+            return dp[i][isEven];
         }
         //not take
-        long long notTake = solve(nums, ind+1, isEven, dp);
+        long long notTake = solve(nums, i+1, isEven, dp);
 
         //take
-        long long take=0;
+        long long take = 0;
+        //if even then add 
         if(isEven){
-            take = nums[ind] + solve(nums, ind + 1, false,dp);
-        }
-        else{
-            take = -nums[ind] + solve(nums, ind + 1, true,dp);
+            take = nums[i] + solve(nums, i+1, false, dp);
+        }else{
+            take = -nums[i] + solve(nums, i+1, true, dp);
         }
 
-        
-        return dp[ind][isEven]=max(take , notTake);
+        return dp[i][isEven] = max(notTake, take);
     }
-
     long long maxAlternatingSum(vector<int>& nums) {
+        bool isEven = true;
         int n = nums.size();
-
-        vector<vector<long long>>dp(n,vector<long long>(2,-1));
-        return solve(nums, 0, true, dp);
-    }
+        vector<vector<long long>>dp(n, vector<long long>(2, -1));
+        return solve(nums, 0, isEven, dp);
+    }   
 };
