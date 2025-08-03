c4c9342b94a91eb0d2dc691e4277b7c959610392
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-08-03 23:51:34 +0530

    Time: 17 ms (13.29%) | Memory: 11.4 MB (10.44%) - LeetSync

---
+++ 1187-print-foobar-alternately/print-foobar-alternately.cpp
@@ -0,0 +1,37 @@
+class FooBar {
+private:
+    int n;
+    bool isFooPrinted;
+    mutex mtx;
+    condition_variable cv;
+
+public:
+    FooBar(int n) {
+        this->n = n;
+        isFooPrinted = false;
+    }
+
+    void foo(function<void()> printFoo) {
+        
+        for (int i = 0; i < n; i++) {
+            unique_lock<mutex> lock(mtx);
+            cv.wait(lock, [this]{return !isFooPrinted;});
+        	// printFoo() outputs "foo". Do not change or remove this line.
+        	printFoo();
+            isFooPrinted = true;
+            cv.notify_all();
+        }
+    }
+
+    void bar(function<void()> printBar) {
+        
+        for (int i = 0; i < n; i++) {
+            unique_lock<mutex> lock(mtx);
+            cv.wait(lock, [this]{return isFooPrinted;});
+        	printBar();
+            isFooPrinted = false;
+            cv.notify_all();
+        	
+        }
+    }
+};
