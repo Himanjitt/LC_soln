b9f38470e07f3d0258126eb19d1e892d03722419
Himanjit Tamuli <121948469+Himanjitt@users.noreply.github.com>
2025-08-11 18:40:08 +0530

    Time: 3 ms (62.04%) | Memory: 18.4 MB (31.24%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,51 +1,50 @@
 class Solution {
 public:
-    // normal bfs with maximum levels possible as K and relaxation
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
-                          int K) {
+                          int k) {
         map<int, vector<pair<int, int>>> adj;
-        for (auto ele : flights) {
-            int u = ele[0];
-            int v = ele[1];
-            int wt = ele[2];
+
+        for (auto edge : flights) {
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
 
             adj[u].push_back({v, wt});
         }
-        vector<int> distance(n, INT_MAX);
-        distance[src] = 0;
-        queue<pair<int, int>> q;
-        int levels = 0;
 
+        queue<pair<int, int>>q;
+
+        vector<int> dist(n, INT_MAX);
+        dist[src] = 0;
         q.push({src, 0});
 
+        int level = 0;
         while (!q.empty()) {
             int size = q.size();
 
-            if(levels > K){
+            if (level > k) {
                 break;
             }
             while (size--) {
-                auto node = q.front();
+                auto top = q.front();
                 q.pop();
-
-                int u = node.first;
-                int dis = node.second;
+                
+                int u = top.first;
+                int u_wt = top.second;
 
                 for (auto neighbor : adj[u]) {
                     int v = neighbor.first;
                     int wt = neighbor.second;
 
-                    if (wt + dis < distance[v]) {
-                        distance[v] = wt + dis;
-                        q.push({v, distance[v]});
+                    if (u_wt + wt < dist[v]) {
+                        dist[v] = u_wt + wt;
+                        q.push({v, dist[v]});
                     }
                 }
             }
-            levels++;
+            level++;
         }
+        return dist[dst] == INT_MAX ? -1 : dist[dst];
 
-        if (distance[dst] == INT_MAX)
-            return -1;
-        return distance[dst];
     }
 };
